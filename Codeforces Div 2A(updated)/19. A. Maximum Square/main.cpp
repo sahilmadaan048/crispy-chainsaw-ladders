dbb358ea9cb6e5c3e401245e564b8fec37f862ea
Sahil Madaan <madaan.sahil27@gmail.com>
2025-02-27 02:11:58 +0530

    done rating 800

---
+++ Codeforces Div 2A(updated)/19. A. Maximum Square/main.cpp
@@ -0,0 +1,60 @@
+// https://codeforces.com/problemset/problem/1243/A
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll             long long int 
+#define ulli           unsigned long long int 
+#define li             long int 
+#define ff(i,a,b)      for(int i=a;i<b;i++)
+#define fb(i,b,a)      for(int i=b;i>=a;i--)
+#define w(t)           while(--t >= 0)
+#define l(s)           s.length()
+#define ci(n)          cin>>n;
+#define fast           ios_base::sync_with_stdio(false);
+#define sa(a,n)        sort(a,a+n)
+#define sv(v)          sort(v.begin(),v.end())
+#define cy             cout<<"YES\n"
+#define cn             cout<<"NO\n"
+#define nl             cout<<"\n"
+#define minus          cout<<"-1\n";
+#define vi             vector<int>
+#define pb             push_back
+#define tc             int t; cin>>t;
+#define pp             pair<int,int>
+#define input(a,n)     for(int i=0;i<n;i++) cin>>a[i];
+#define mod            1000000007
+#define co(n)          cout<<n;
+#define ret            return 0
+#define mi             map<int,int>
+#define output(a,n)    for(int i=0;i<n;i++) cout<<a[i]<<" ";   
+#define forn(i, n)     ff(i, 0, n)
+#define sz(v)          int((v).size())
+
+void solve(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    
+    sort(a.rbegin(), a.rend()); // Sort in descending order
+    int max_side = 0;
+    
+    for (int i = 0; i < n; i++) {
+        int side_length = min(a[i], i + 1); // The max square side possible with (i+1) planks
+        max_side = max(max_side, side_length);
+    }
+    
+    cout << max_side << "\n";
+}
+
+int main(){
+	fast;
+	int t; cin >> t;
+	while(t--){
+		solve();
+	}
+	return 0;
+}
