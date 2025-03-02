52c205a1a24ee3b83651b520688da67d1ab38bd6
Sahil Madaan <madaan.sahil27@gmail.com>
2025-03-02 16:03:28 +0530

    done rating 900

---
+++ Codeforces Div 2A(updated)/40. A. Be Positive/main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    
+    int required = (n + 1) / 2; // ceil(n/2)
+    
+    for (int d = -1000; d <= 1000; d++) {
+        if (d == 0) continue; // d must be non-zero
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if ((double)a[i] / d > 0) {
+                count++;
+            }
+        }
+        if (count >= required) {
+            cout << d << endl;
+            return 0;
+        }
+    }
+    
+    cout << 0 << endl;
+    return 0;
+}
