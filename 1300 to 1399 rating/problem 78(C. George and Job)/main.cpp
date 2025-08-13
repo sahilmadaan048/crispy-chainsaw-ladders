95a5b8618dfd2c9b22e36f14970d22ed79516fac
Sahil Madaan <madaan.sahil27@gmail.com>
2025-08-13 19:11:37 +0530

    C. George and Job

--- 1300 to 1399 rating/problem 78(C. George and Job)/main.cpp
+++ 1300 to 1399 rating/problem 78(C. George and Job)/main.cpp
@@ -1,47 +1,101 @@
+// sahilmadaan048
 // https://codeforces.com/problemset/problem/467/C
 
-#include <bits/stdc++.h>
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())         // sort 
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed 
+#define bin(x,y)  bitset<y>(x) 
 using namespace std;
-#define ll             long long int 
-#define ulli           unsigned long long int 
-#define li             long int 
-#define ff(i,a,b)      for(int i=a;i<b;i++)
-#define fb(i,b,a)      for(int i=b;i>=a;i--)
-#define w(t)           while(--t >= 0)
-#define ci(n)          cin>>n;
-#define fast           ios_base::sync_with_stdio(false);
-#define sa(a,n)        sort(a,a+n)
-#define sv(v)          sort(v.begin(),v.end())
-#define cy             cout<<"YES\n"
-#define cn             cout<<"NO\n"
-#define nl             cout<<"\n"
-#define minus          cout<<"-1\n";
-#define vi             vector<int>
-#define pb             push_back
-#define tc             int t; cin>>t;
-#define pp             pair<int,int>
-#define input(a,n)     for(int i=0;i<n;i++) cin>>a[i];
-#define mod            1000000007
-#define co(n)          cout<<n;
-#define ret            return 0
-#define mi             map<int,int>
-#define output(a,n)    for(int i=0;i<n;i++) cout<<a[i]<<" ";   
-#define forn(i, n)     ff(i, 0, n)
-#define sz(v)          int((v).size())
+int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
+
+
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+// ================================== take ip/op like vector,pairs directly!==================================
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ==========================================
+
 
 void solve(){
-	int n,m,k; cin >> n >> m >> k;
-	vi temp(m);
-	for(int i=0; i<m; i++) cin>>temp[i];	
-	
-	return;
+	int n, m, k; cin >> n >> m >> k;
+	vi p(n); 
+
+	fr(i, n) {
+		cin >> p[i];
+	}
+
+	vector<int> segSum(n - m + 1);
+
+	int sum = 0;
+
+	fr(i, m) {
+		sum += p[i];
+	}
+	segSum[0] = sum;
+
+	for(int i=m; i<n; i++) {
+		sum += p[i] - p[i-m]; // new element added at last and the first element of the previous segment removed 
+		segSum[i - m + 1] = sum;
+	}
+
+
+	// at eachplace we are storing, dp[i][[j]] = max sum we can get from the first i elementm, using k segments
+	vector<vector<int>> dp(n+1, vector<int>(k+1, 0)); 
+
+
+	for(int i=1; i<=n; i++) {
+		for(int j=0; j<=k; j++) {
+			// not take
+			dp[i][j] = dp[i-1][j];
+
+			// take only ifi>=m and j.>=
+			// tak segemnt fo length m ending at i only if 
+			if(i >= m and j  >= 1) {
+				dp[i][j] = max(dp[i][j], dp[i-m][j-1] + segSum[i-m]);
+			}
+		}
+	}
+
+
+	cout << dp[n][k] << endl;
+
+
 }
 
-int main(){
-	fast;
-	int t=1;
-	while(t--){
+int32_t main()
+{
+ 
+ ios_base::sync_with_stdio(false);
+ cin.tie(NULL);
+
+	int T = 1;
+	// cin >> T;
+	while (T--)
+	{
 		solve();
 	}
 	return 0;
 }
+
+	
