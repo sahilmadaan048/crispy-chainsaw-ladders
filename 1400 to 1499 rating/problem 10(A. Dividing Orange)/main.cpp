ea02568c0718b0bcd8ffb9eec121ab5c88b82247
Sahil Madaan <madaan.sahil27@gmail.com>
2025-08-18 21:45:18 +0530

    A. Dividing Orange

---
+++ 1400 to 1499 rating/problem 10(A. Dividing Orange)/main.cpp
@@ -0,0 +1,92 @@
+// https://codeforces.com/problemset/problem/244/A
+
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())         // sort 
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed 
+#define bin(x,y)  bitset<y>(x) 
+using namespace std;
+int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
+
+
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+// ================================== take ip/op like vector,pairs directly!==================================
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ==========================================
+
+
+void solve() {
+    int n, k; 
+    cin >> n >> k;
+
+    vector<int> a(k);
+    set<int> chosen;
+
+    for(int i = 0; i < k; i++) {
+        cin >> a[i];
+        chosen.insert(a[i]);
+    }
+
+    // n indexes of the segments child 1 will get
+    // n indexes of the segments child 2 will get
+    // n indexes of the segments child 3 will get
+    // .
+    // .
+    // .
+    // .
+    // n indexes of the segments child k will get
+
+    vector<int> B;
+    for(int i = 1; i <= n*k; i++) {
+        if(chosen.find(i) == chosen.end()) {
+            B.push_back(i);
+        }
+    }
+
+    int idx = 0;
+    for(int i = 0; i < k; i++) {
+        cout << a[i] << " ";
+        for(int j = 0; j < n-1; j++) {
+            cout << B[idx++] << " ";
+        }
+        cout << "\n";
+    }
+}
+
+int32_t main()
+{
+ 
+ ios_base::sync_with_stdio(false);
+ cin.tie(NULL);
+
+    int T = 1;
+    // cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+    
