aae4d1fc832461bec1b69a58ab844002c877e8ca
Sahil Madaan <madaan.sahil27@gmail.com>
2025-10-27 23:25:33 +0530

    B. Physics Practical

---
+++ 1400 to 1499 rating/problem 90.(B. Physics Practical)/main2.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned int ui;
+typedef unsigned long long ull;
+
+#define Rep(i,n) for(int i = 0; i < (n); ++i)
+#define Repd(i,n) for(int i = (n)-1; i >= 0; --i)
+#define For(i,a,b) for(int i = (a); i <= (b); ++i)
+#define Ford(i,a,b) for(int i = (a); i >= (b); --i)
+#define Fit(i,v) for(__typeof((v).begin()) i = (v).begin(); i != (v).end(); ++i)
+#define Fitd(i,v) for(__typeof((v).rbegin()) i = (v).rbegin(); i != (v).rend(); ++i)
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define sz(a) ((int)(a).size())
+#define all(a) (a).begin(), (a).end()
+#define ms(a,x) memset(a, x, sizeof(a))
+
+template<class F, class T> T convert(F a, int p = -1) { stringstream ss; if (p >= 0) ss << fixed << setprecision(p); ss << a; T r; ss >> r; return r; }
+template<class T> T gcd(T a, T b) { T r; while (b != 0) { r = a % b; a = b; b = r; } return a; }
+template<class T> T lcm(T a, T b) { return a / gcd(a, b) * b; }
+template<class T> T sqr(T x) { return x * x; }
+template<class T> T cube(T x) { return x * x * x; }
+template<class T> int getbit(T s, int i) { return (s >> i) & 1; }
+template<class T> T onbit(T s, int i) { return s | (T(1) << i); }
+template<class T> T offbit(T s, int i) { return s & (~(T(1) << i)); }
+template<class T> int cntbit(T s) { return s == 0 ? 0 : cntbit(s >> 1) + (s & 1); }
+
+const int bfsz = 1 << 16; char bf[bfsz + 5]; int rsz = 0;int ptr = 0;
+char gc() { if (rsz <= 0) { ptr = 0; rsz = fread(bf, 1, bfsz, stdin); if (rsz <= 0) return EOF; } --rsz; return bf[ptr++]; }
+void ga(char &c) { c = EOF; while (!isalpha(c)) c = gc(); }
+int gs(char s[]) { int l = 0; char c = gc(); while (isspace(c)) c = gc(); while (c != EOF && !isspace(c)) { s[l++] = c; c = gc(); } s[l] = '\0'; return l; }
+template<class T> bool gi(T &v) {
+	v = 0; char c = gc(); while (c != EOF && c != '-' && !isdigit(c)) c = gc(); if (c == EOF) return false; bool neg = c == '-'; if (neg) c = gc();
+    while (isdigit(c)) { v = v * 10 + c - '0'; c = gc(); } if (neg) v = -v; return true;
+}
+
+typedef pair<int, int> II;
+
+const double PI = acos(-1.0);
+const double eps = 1e-9;
+
+const int dr[] = {-1, 0, +1, 0};
+const int dc[] = {0, +1, 0, -1};
+
+const int inf = (int)1e9 + 5;
+const ll linf = (ll)1e16 + 5;
+const ll mod = (ll)1e9 + 7;
+
+#define maxn 200005
+
+int n, a[maxn];
+
+int main() {
+
+	cin >> n;
+	For(i, 1, n) cin >> a[i];
+
+	sort(a + 1, a + n + 1);
+	int res = inf;
+
+	int j = 1;
+	For(i, 1, n) {
+		if (j < i) j = i;
+		while (j <= n && a[j] <= 2 * a[i]) ++j;
+		res = min(res, i + n - j);
+	}
+
+	cout << res << endl;
+
+
+	return 0;
+}
