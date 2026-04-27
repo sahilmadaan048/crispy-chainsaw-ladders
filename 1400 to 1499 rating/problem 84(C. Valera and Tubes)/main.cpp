b88ff1b4042f72371498dd24bb6a1fe7080ef968
Sahil Madaan <madaan.sahil27@gmail.com>
2026-04-27 21:03:19 +0530

    84. Valera and Tubes

---
+++ 1400 to 1499 rating/problem 84(C. Valera and Tubes)/main.cpp
@@ -0,0 +1,92 @@
+// https://codeforces.com/problemset/problem/441/C
+// Author - sahilmadaan048
+
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())         // sort 
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed 
+#define bin(x,y)  bitset<y>(x) 
+using namespace std;
+int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
+
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+// ================================== take ip/op like vector,pairs directly!==================================
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ==========================================
+
+
+void solve() {
+    int n,m,k;
+    cin >> n >> m >> k;
+
+    vector<pair<int,int>> path;
+
+    // Snake traversal of grid
+    for(int i=0;i<n;i++){
+        if(i%2==0){
+            for(int j=0;j<m;j++)
+                path.push_back({i+1,j+1});
+        }
+        else{
+            for(int j=m-1;j>=0;j--)
+                path.push_back({i+1,j+1});
+        }
+    }
+
+    int idx = 0;
+
+    // First k-1 groups get exactly 2 cells
+    for(int g=1; g<k; g++){
+        cout << 2 << " ";
+        cout << path[idx].first << " " << path[idx].second << " ";
+        idx++;
+
+        cout << path[idx].first << " " << path[idx].second;
+        idx++;
+
+        cout << "\n";
+    }
+
+    // Last group gets remaining cells
+    cout << (sz(path)-idx) << " ";
+    while(idx < sz(path)){
+        cout << path[idx].first << " " << path[idx].second;
+        idx++;
+        if(idx < sz(path)) cout << " ";
+    }
+    cout << "\n";
+}
+int32_t main()
+{
+ 
+ ios_base::sync_with_stdio(false);
+ cin.tie(NULL);
+
+    int T = 1;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
