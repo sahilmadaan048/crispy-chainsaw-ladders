5d48b9d0449f0b70a96664be1423264d2b7cb725
Sahil Madaan <madaan.sahil27@gmail.com>
2026-04-30 10:41:17 +0530

    39. Booking System

--- 1500 to 1599 rating/problem 39(C. Booking System)/main.cpp
+++ 1500 to 1599 rating/problem 39(C. Booking System)/main.cpp
@@ -1,185 +1,112 @@
 
+// https://codeforces.com/problemset/problem/416/C
+// Author - sahilmadaan048
+
 #include "bits/stdc++.h"
 #define int long long
 #define uint unsigned long long
 #define vi vector<int>
-#define vvi vector<vi>
+#define vvi vector<vi >
 #define vb vector<bool>
-#define vvb vector<vb>
-#define fr(i, n) for (int i = 0; i < (n); i++)
-#define rep(i, a, n) for (int i = (a); i <= (n); i++)
-#define nl cout << "\n"
-#define dbg(var) cout << #var << "=" << var << " "
-#define all(v) v.begin(), v.end()
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
 #define sz(v) (int)(v.size())
-#define srt(v) sort(v.begin(), v.end())         // sort
-#define mxe(v) *max_element(v.begin(), v.end()) // find max element in vector
-#define mne(v) *min_element(v.begin(), v.end()) // find min element in vector
-#define unq(v) v.resize(distance(v.begin(), unique(v.begin(), v.end())));
-// make sure to sort before applying unique // else only consecutive duplicates
-// would be removed
-#define bin(x, y) bitset<y>(x)
+#define srt(v)  sort(v.begin(),v.end())         // sort 
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed 
+#define bin(x,y)  bitset<y>(x) 
 using namespace std;
-int MOD = 1e9 + 7; // Hardcoded, directly change from here for functions!
+int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
 
-void modadd(int &a, int b) { a = ((a % MOD) + (b % MOD)) % MOD; }
-void modsub(int &a, int b) { a = ((a % MOD) - (b % MOD) + MOD) % MOD; }
-void modmul(int &a, int b) { a = ((a % MOD) * (b % MOD)) % MOD; }
-// ================================== take ip/op like vector,pairs
-// directly!==================================
-template <typename typC, typename typD>
-istream &operator>>(istream &cin, pair<typC, typD> &a) {
-  return cin >> a.first >> a.second;
-}
-template <typename typC> istream &operator>>(istream &cin, vector<typC> &a) {
-  for (auto &x : a)
-    cin >> x;
-  return cin;
-}
-template <typename typC, typename typD>
-ostream &operator<<(ostream &cout, const pair<typC, typD> &a) {
-  return cout << a.first << ' ' << a.second;
-}
-template <typename typC, typename typD>
-ostream &operator<<(ostream &cout, const vector<pair<typC, typD>> &a) {
-  for (auto &x : a)
-    cout << x << '\n';
-  return cout;
-}
-template <typename typC>
-ostream &operator<<(ostream &cout, const vector<typC> &a) {
-  int n = a.size();
-  if (!n)
-    return cout;
-  cout << a[0];
-  for (int i = 1; i < n; i++)
-    cout << ' ' << a[i];
-  return cout;
-}
-// ===================================END Of the input module
-// ==========================================
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+// ================================== take ip/op like vector,pairs directly!==================================
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ==========================================
+
 
 void solve() {
-  int n;
-  cin >> n;
+    int n;
+    cin >> n;
 
-  vector<pair<int, int>> temp;
-  int c, p;
+  /*
 
-  for (int i = 0; i < n; i++) {
-    cin >> c >> p;
-    temp.push_back({c, p});
-  }
+    m => no of acc req
+    s => total money
 
-  int k;
-  cin >> k;
 
-#include "bits/stdc++.h"
-#define int long long
-#define uint unsigned long long
-#define vi vector<int>
-#define vvi vector<vi>
-#define vb vector<bool>
-#define vvb vector<vb>
-#define fr(i, n) for (int i = 0; i < (n); i++)
-#define rep(i, a, n) for (int i = (a); i <= (n); i++)
-#define nl cout << "\n"
-#define dbg(var) cout << #var << "=" << var << " "
-#define all(v) v.begin(), v.end()
-#define sz(v) (int)(v.size())
-#define srt(v) sort(v.begin(), v.end())         // sort
-#define mxe(v) *max_element(v.begin(), v.end()) // find max element in vector
-#define mne(v) *min_element(v.begin(), v.end()) // find min element in vector
-#define unq(v) v.resize(distance(v.begin(), unique(v.begin(), v.end())));
-// make sure to sort before applying unique // else only consecutive duplicates
-// would be removed
-#define bin(x, y) bitset<y>(x)
-  using namespace std;
-  int MOD = 1e9 + 7; // Hardcoded, directly change from here for functions!
+    -----m lines ----
 
-  void modadd(int &a, int b) { a = ((a % MOD) + (b % MOD)) % MOD; }
-  void modsub(int &a, int b) { a = ((a % MOD) - (b % MOD) + MOD) % MOD; }
-  void modmul(int &a, int b) { a = ((a % MOD) * (b % MOD)) % MOD; }
-  // ================================== take ip/op like vector,pairs
-  // directly!==================================
-  template <typename typC, typename typD>
-  istream &operator>>(istream &cin, pair<typC, typD> &a) {
-    return cin >> a.first >> a.second;
-  }
-  template <typename typC> istream &operator>>(istream &cin, vector<typC> &a) {
-    for (auto &x : a)
-      cin >> x;
-    return cin;
-  }
-  template <typename typC, typename typD>
-  ostream &operator<<(ostream &cout, const pair<typC, typD> &a) {
-    return cout << a.first << ' ' << a.second;
-  }
-  template <typename typC, typename typD>
-  ostream &operator<<(ostream &cout, const vector<pair<typC, typD>> &a) {
-    for (auto &x : a)
-      cout << x << '\n';
-    return cout;
-  }
-  template <typename typC>
-  ostream &operator<<(ostream &cout, const vector<typC> &a) {
-    int n = a.size();
-    if (!n)
-      return cout;
-    cout << a[0];
-    for (int i = 1; i < n; i++)
-      cout << ' ' << a[i];
-    return cout;
-  }
-  // ===================================END Of the input module
-  // ==========================================
+    (a, b) => ( number of the accepted request , no of table to seat peopke that came via this request)
+  
+  */
 
-  void solve() {
-    string s1, s2;
-    cin >> s1 >> s2;
 
-    transform(s1.begin(), s1.end(), s1.begin(), ::tolower);
-    transform(s2.begin(), s2.end(), s2.begin(), ::tolower);
+    vector<tuple<int,int,int>> groups; 
+    // {money, size, original_index}
 
-    int result = s1.compare(s2);
-
-    if (result < 0) {
-      cout << "-1";
-    } else if (result > 0) {
-      cout << "1";
-    } else {
-      cout << "0";
+    for (int i = 0; i < n; i++) {
+        int c, p;
+        cin >> c >> p;
+        groups.push_back({p, c, i + 1});
     }
-  }
 
-  int32_t main() {
+    int k;
+    cin >> k;
 
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    multiset<pair<int,int>> tables;
+    // {capacity, table_index}
 
-    int T = 1;
-    while (T--) {
-      solve();
+    for (int i = 0; i < k; i++) {
+        int r;
+        cin >> r;
+        tables.insert({r, i + 1});
     }
-    return 0;
-  }
 
-  vector<int> r(k);
+    sort(groups.begin(), groups.end(), greater<>());
 
-  for (int i = 0; i < n; i++)
-    cin >> r[i];
+    long long total_money = 0;
+    vector<pair<int,int>> answer;
 
-  cout << "sahil madaan is my name\n";
+    for (auto &[money, size, idx] : groups) {
+
+        auto it = tables.lower_bound({size, 0});
+        if (it == tables.end()) continue;
+
+        total_money += money;
+        answer.push_back({idx, it->second});
+        tables.erase(it);
+    }
+
+    cout << answer.size() << " " << total_money << "\n";
+    for (auto &[g, t] : answer) {
+        cout << g << " " << t << "\n";
+    }
 }
 
-int32_t main() {
 
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
+int32_t main()
+{
+ 
+ ios_base::sync_with_stdio(false);
+ cin.tie(NULL);
 
-  int T = 1;
-  while (T--) {
-    solve();
-  }
-  return 0;
+    int T = 1;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
 }
